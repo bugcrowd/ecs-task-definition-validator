94b80e6ae38055c665bc8734232129892b190905
Coen Hyde <coen.hyde@gmail.com>
2018-08-03 12:11:33 -0500

    add test to verify missing fields in task definitions

--- test/index.js
+++ test/index.js
@@ -6,6 +6,11 @@ const validator = require('../');
 const fixtures = require('./fixtures');
 
 describe('EcsTaskDefinitionValidator', function() {
+  it('should return errors when validating a Task Definition with missing fields', function() {
+    let result = validator(fixtures['missingFieldsTaskDefinition']);
+    expect(result.errors.length).to.be.greaterThan(0);
+  });
+
   it('should validate a valid Task Definition', function() {
     let result = validator(fixtures['validTaskDefinition']);
     expect(result.errors.length).to.equal(0);
